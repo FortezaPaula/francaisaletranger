c7f0b123a89402376591f3f580e05caf1bce102c
Xavier Julien <xjulien@octo.com>
2020-03-24 13:46:23 +0100

    Upgrade admin page with new API needHelps allWithMatching

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -10,10 +10,10 @@
           Nombre de personne pouvant aider
         </th>
       </tr>
-      <tr v-for="(needHelp) in needHelpListConso" :key="needHelp.id">
+      <tr v-for="(needHelp) in needHelpList" :key="needHelp.id">
         <td>{{ needHelp.nom }}</td>
         <td>{{ needHelp.prenom }}</td>
-        <td>{{ needHelp.score === undefined ? 'Chargement ...' : needHelp.score }}</td>
+        <td>{{ needHelp.score}}</td>
         <td>
           <nuxt-link :to="'/admin/need-helps/'+needHelp.id">
             Voir la demande
@@ -38,56 +38,15 @@
       }
     },
 
-    computed: {
-      needHelpListConso () {
-        return this.needHelpList.filter(needHelp => needHelp.score > 0 || needHelp.score === undefined)
-      }
-    },
-
     mounted () {
-      axios.get('/api/need-help', {
+      axios.get('/api/need-help/allWithMatching', {
         params: {
           access_token: accessToken(),
-          filter: {
-            where: {
-              helper_id: null
-            },
-            limit: 50
-          }
+          maxDistance: 10000
         }
       }).then((response) => {
-        this.needHelpList = response.data.map((needHelp) => {
-          return { ...needHelp, score: undefined }
-        })
-
-        this.needHelpListScore = response.data.map((needHelp) => {
-          return needHelp.id.toString()
-        })
-
-        this.needHelpListScore.reduce(async (previousPromise, nextID) => {
-          await previousPromise
-          return this.matchPotentiel(nextID)
-        }, Promise.resolve())
+        this.needHelpList = response.data
       })
-    },
-
-    methods: {
-      async matchPotentiel (needHelpID) {
-        const response = await axios.get('/api/matching/', {
-          params: {
-            id: needHelpID,
-            maxDistance: 10000,
-            access_token: accessToken()
-          }
-        })
-
-        this.needHelpList.find(needHelp => needHelp.id === parseInt(needHelpID)).score = response.data.length
-
-        return response.data.length
-      }
     }
   }
 </script>
-
-<style lang="scss">
-</style>
