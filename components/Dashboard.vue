e607ac77a0b5e384f1b0adcf7a84339037fefde3
Guillaume Fabrizi <gfabrizi@octo.com>
2020-03-25 10:33:10 +0100

    #61: Common Dashboard component for need-help and can-help pages

---
+++ components/Dashboard.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="dashboard">
+    <div ref="infoBanner" class="alert alert-info" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="deleteInfoBanner">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      {{ $t('info-banner-user-information') }}
+    </div>
+    <h3>Tableau de bord</h3>
+    <div>
+      Nom : {{ form.nom }}
+    </div>
+    <div>
+      Prénom : {{ form.prenom }}
+    </div>
+    <div>
+      Email : {{ form.email }}
+    </div>
+    <div>
+      <b-form @submit="onSubmit">
+        <b-form-group label="Je peux aider pour :">
+          <b-form-checkbox-group
+            v-model="$v.form.helpFor.selected.$model"
+            :options="form.helpFor.options"
+            value-field="data"
+            text-field="name"
+            stacked
+          />
+          <div v-if="form.helpFor.selected.includes('hebergement')">
+            <br>
+            Logement pour :
+            <b-form-select
+              :options="[
+              {text: '1 personne', value: 1},
+              {text: '2 personnes', value: 2},
+              {text: '3 personnes', value: 3},
+              {text: '4 personnes', value: 4},
+              {text: '5 personnes', value: 5},
+              {text: '6 personnes', value: 6}]"
+              v-model="form.helpFor.hebergement_number"
+              style="width: 30%"></b-form-select>
+          </div>
+        </b-form-group>
+        <br>
+        <div class="button-go">
+          <b-button type="submit" variant="primary">
+            Mettre à jour mes capacités d'aide
+          </b-button>
+          <b-button @click="displayModal()" variant="danger">
+            Supprimer ma proposition d'aide
+          </b-button>
+
+          <b-modal
+            hide-footer
+            ref="delete-help-proposal-modal"
+            title="Je supprime ma proposition d'aide :"
+          >
+            <b-button
+              @click="deleteHelper()"
+              block
+              class="mt-3"
+              variant="primary">
+              Supprimer définitivement
+            </b-button>
+            <b-button
+              @click="hideModal()"
+              block
+              class="mt-2"
+              variant="danger">
+              Annuler
+            </b-button>
+          </b-modal>
+
+          <div v-if="requestSend" class="good-send">
+            Bien reçu !
+          </div>
+        </div>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import availableHelpers from '../helpers/availableHelpers'
+
+  export default {
+    name: 'Dashboard',
+    props: {
+      serverMiddlewareUrl: {
+        type: String,
+        default: ''
+      },
+      typeForm: {
+        type: String,
+        default: ''
+      }
+    },
+    data () {
+      return {
+        requestSend: false,
+        form: {
+          id: undefined,
+          nom: undefined,
+          prenom: undefined,
+          email: undefined,
+          position: {
+            pays: '',
+            ville: '',
+            latitude: null,
+            longitude: null
+          },
+          helpFor: {
+            selected: [],
+            hebergement_number: 1,
+            options: availableHelpers()
+          }
+        }
+      }
+    },
+    validations: {
+      form: {
+        helpFor: {
+          selected: {}
+        }
+      }
+    },
+
+    beforeMount () {
+
+    },
+
+    mounted () {
+      if (!localStorage.getItem('myInfos_' + this.typeForm)) {
+        this.$router.push({ path: '/forms/' + this.typeForm + '-help' })
+      } else {
+        this.form = JSON.parse(localStorage.getItem('myInfos_' + this.typeForm))
+      }
+      this.$refs.infoBanner.style.display = 'block'
+    },
+
+    methods: {
+      availableHelpers,
+      deleteInfoBanner () {
+        this.$refs.infoBanner.style.display = 'none'
+      },
+      onSubmit (event) {
+        event.preventDefault()
+
+        this.$v.form.$touch()
+
+        if (this.$v.form.$anyError) {
+          return
+        }
+
+        if (!this.form.helpFor.selected.includes('hebergement')) {
+          this.form.helpFor.hebergement_number = 0
+        }
+
+        const sendedData = {
+          id: this.form.id,
+          nom: this.form.nom,
+          prenom: this.form.prenom,
+          email: this.form.email,
+          position: this.form.position,
+          gps_coordinates: {
+            lat: this.form.position.latitude,
+            lng: this.form.position.longitude
+          },
+          nombre_hebergement: this.form.helpFor.hebergement_number,
+          approvisionnement: this.form.helpFor.selected.includes('approvisionnement'),
+          garde_enfants: this.form.helpFor.selected.includes('enfants'),
+          autres: this.form.helpFor.selected.includes('autres')
+        }
+
+        axios.put(this.serverMiddlewareUrl, sendedData).then((response) => {
+          if (response.status === 200) {
+            localStorage.setItem('myInfos_' + this.typeForm, JSON.stringify(this.form))
+            this.requestSend = true
+          }
+        })
+      },
+      displayModal () {
+        this.$refs['delete-help-proposal-modal'].show()
+      },
+      hideModal () {
+        this.$refs['delete-help-proposal-modal'].hide()
+      },
+      deleteHelper () {
+        axios.delete(this.serverMiddlewareUrl + '/' + this.form.id).then((response) => {
+          if (response.status === 200) {
+            localStorage.removeItem('myInfos_' + this.typeForm)
+            this.hideModal()
+            this.$router.push({ path: '/' })
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .alert {
+    display: none;
+  }
+
+  .dashboard {
+    padding: 10px;
+  }
+</style>
--- pages/need-help/dashboard.vue
+++ pages/need-help/dashboard.vue
@@ -1,200 +1,32 @@
 <template>
-  <div class="dashboard">
-    <div ref="infoBanner" class="alert alert-info" role="alert">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="deleteInfoBanner">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      {{ $t('info-banner-user-information') }}
-    </div>
-    <h3>Tableau de bord</h3>
-    <div>
-      Nom : {{ form.nom }}
-    </div>
-    <div>
-      Prénom : {{ form.prenom }}
-    </div>
-    <div>
-      Email : {{ form.email }}
-    </div>
-    <div>
-      <b-form @submit="onSubmit">
-        <b-form-group label="J'ai besoin d'aide pour :">
-          <b-form-checkbox-group
-            v-model="$v.form.helpFor.selected.$model"
-            :options="form.helpFor.options"
-            value-field="data"
-            text-field="name"
-            stacked
-          />
-          <div v-if="form.helpFor.selected.includes('hebergement')">
-            <br>
-            Logement pour :
-            <b-form-select
-              :options="[
-              {text: '1 personne', value: 1},
-              {text: '2 personnes', value: 2},
-              {text: '3 personnes', value: 3},
-              {text: '4 personnes', value: 4},
-              {text: '5 personnes', value: 5},
-              {text: '6 personnes', value: 6}]"
-              v-model="form.helpFor.hebergement_number"
-              style="width: 30%"></b-form-select>
-          </div>
-        </b-form-group>
-        <br>
-        <div class="button-go">
-          <b-button type="submit" variant="primary">
-            Mettre à jour mes besoins d'aide
-          </b-button>
-          <b-button @click="displayModal()" variant="danger">
-            Supprimer ma demande d'aide
-          </b-button>
-
-          <b-modal
-            hide-footer
-            ref="delete-help-proposal-modal"
-            title="Je supprime ma demande d'aide :"
-          >
-            <b-button
-              @click="deleteHelper()"
-              block
-              class="mt-3"
-              variant="primary">
-              Supprimer définitivement
-            </b-button>
-            <b-button
-              @click="hideModal()"
-              block
-              class="mt-2"
-              variant="danger">
-              Annuler
-            </b-button>
-          </b-modal>
-
-          <div v-if="requestSend" class="good-send">
-            Bien reçu !
-          </div>
-        </div>
-      </b-form>
-    </div>
+  <div class="form-can-help">
+    <BackToHome />
+    <Dashboard
+      server-middleware-url="/api/need-help"
+      type-form="need"
+    />
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
-  import availableHelpers from '../../helpers/availableHelpers'
+  import Dashboard from '@/components/Dashboard'
+  import BackToHome from '@/components/BackToHome'
 
   export default {
-    name: 'Dashboard',
-    data () {
-      return {
-        requestSend: false,
-        serverMiddlewareUrl: '/api/need-help',
-        form: {
-          id: undefined,
-          nom: undefined,
-          prenom: undefined,
-          email: undefined,
-          position: {
-            pays: '',
-            ville: '',
-            latitude: null,
-            longitude: null
-          },
-          helpFor: {
-            selected: [],
-            hebergement_number: 1,
-            options: availableHelpers()
-          }
-        }
-      }
-    },
-    validations: {
-      form: {
-        helpFor: {
-          selected: {}
-        }
-      }
-    },
 
-    beforeMount () {
-      if (!localStorage.myInfos_need) {
-        this.$router.push({ path: '/forms/need-help' })
-      } else {
-        this.form = JSON.parse(localStorage.myInfos_need)
-      }
-    },
-
-    mounted () {
-      this.$refs.infoBanner.style.display = 'block'
-    },
-
-    methods: {
-      availableHelpers,
-      deleteInfoBanner () {
-        this.$refs.infoBanner.style.display = 'none'
-      },
-      onSubmit (event) {
-        event.preventDefault()
-
-        this.$v.form.$touch()
-
-        if (this.$v.form.$anyError) {
-          return
-        }
-
-        if (!this.form.helpFor.selected.includes('hebergement')) {
-          this.form.helpFor.hebergement_number = 0
-        }
-
-        const sendedData = {
-          id: this.form.id,
-          nom: this.form.nom,
-          prenom: this.form.prenom,
-          email: this.form.email,
-          position: this.form.position,
-          gps_coordinates: {
-            lat: this.form.position.latitude,
-            lng: this.form.position.longitude
-          },
-          nombre_hebergement: this.form.helpFor.hebergement_number,
-          approvisionnement: this.form.helpFor.selected.includes('approvisionnement'),
-          garde_enfants: this.form.helpFor.selected.includes('enfants'),
-          autres: this.form.helpFor.selected.includes('autres')
-        }
-
-        axios.put(this.serverMiddlewareUrl, sendedData).then((response) => {
-          if (response.status === 200) {
-            localStorage.setItem('myInfos_need', JSON.stringify(this.form))
-            this.requestSend = true
-          }
-        })
-      },
-      displayModal () {
-        this.$refs['delete-help-proposal-modal'].show()
-      },
-      hideModal () {
-        this.$refs['delete-help-proposal-modal'].hide()
-      },
-      deleteHelper () {
-        axios.delete(this.serverMiddlewareUrl + '/' + this.form.id).then((response) => {
-          if (response.status === 200) {
-            localStorage.removeItem('myInfos_need')
-            this.hideModal()
-            this.$router.push({ path: '/' })
-          }
-        })
-      }
-    }
+    components: { BackToHome, Dashboard }
   }
 </script>
 
 <style lang="scss" scoped>
-  .alert {
-    display: none;
-  }
-
-  .dashboard {
+  .form-can-help {
     padding: 10px;
+    max-width: 900px;
+    margin: auto;
+
+    .form {
+      max-width: 500px;
+      margin: 20px auto auto;
+    }
   }
 </style>
