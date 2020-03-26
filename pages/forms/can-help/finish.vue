d13826b2ca18c0fe9feb32e696d4f46ca59092a6
Julien Jakubowski <jjakubowski@octo.com>
2020-03-26 18:23:50 +0100

    #46 fix graphic design on form: background & standard back-to-home link on finish pages

--- pages/forms/can-help/finish.vue
+++ pages/forms/can-help/finish.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="can-help-done">
-    <h2>Merci pour votre participation.</h2>
-    <br>
-    <p>
-      Si une personne ayant les besoins que vous proposez est proche de vous, elle pourra vous contacter directement par e-mail.
-    </p>
-    <p>
-      En vous souhaitant une très bonne journée.
-    </p>
-    <nuxt-link to="/">
-      <b-button variant="primary">
-        {{ $t('finish-back-to-home-page') }}
-      </b-button>
-    </nuxt-link>
+    <div class="cadre">
+      <h2>Merci pour votre participation.</h2>
+      <br>
+      <p>
+        Si une personne ayant les besoins que vous proposez est proche de vous, elle pourra vous contacter directement par e-mail.
+      </p>
+      <p>
+        En vous souhaitant une très bonne journée.
+      </p>
+      <br/>
+      <BackToHome/>
+    </div>
   </div>
 </template>
 
 <script>
+  import BackToHome from '../../../components/BackToHome'
   export default {
-    name: 'Finish'
+    name: 'Finish',
+    components: { BackToHome }
   }
 </script>
 
@@ -26,6 +27,5 @@
   .can-help-done {
     max-width: 700px;
     margin: auto;
-    padding: 10px;
   }
 </style>
--- pages/forms/need-help/finish.vue
+++ pages/forms/need-help/finish.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="can-help-done">
-    <h2>Merci, nous avons bien reçu votre demande.</h2>
-    <br>
-    <p>
-      Un contact par mail sera établi dès qu'une personne à proximité se déclare disponible pour vous aider.
-    </p>
-    <nuxt-link to="/">
-      <b-button variant="primary">
-        {{ $t('finish-back-to-home-page') }}
-      </b-button>
-    </nuxt-link>
+    <div class="cadre">
+      <h2>Merci, nous avons bien reçu votre demande.</h2>
+      <br>
+      <p>
+        Un contact par mail sera établi dès qu'une personne à proximité se déclare disponible pour vous aider.
+      </p>
+      <br/>
+      <BackToHome/>
+    </div>
   </div>
 </template>
 
 <script>
+  import BackToHome from '../../../components/BackToHome'
   export default {
-    name: 'Finish'
+    name: 'Finish',
+    components: { BackToHome }
   }
 </script>
 
@@ -23,6 +24,5 @@
   .can-help-done {
     max-width: 700px;
     margin: auto;
-    padding: 10px;
   }
 </style>
