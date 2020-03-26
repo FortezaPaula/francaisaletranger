37d9f573d7805bc1717c3f16740a666a09a242bf
Julien Jakubowski <jjakubowski@octo.com>
2020-03-26 14:46:52 +0100

    #46 fix graphic design on form

--- helpers/availableHelpers.js
+++ helpers/availableHelpers.js
@@ -1,11 +1,11 @@
 export default () => {
   return [
-    { name: 'Une solution de logement', data: 'hebergement' },
     { name: 'Des conseils et de l’appui pour des démarches pratiques', data: 'autres' },
     { name: 'Garder les enfants', data: 'enfants' },
     {
       name: 'Me réapprovisionner en produits de première nécessité et faire des courses à proximité',
       data: 'approvisionnement'
-    }
+    },
+    { name: 'Une solution de logement', data: 'hebergement' }
   ]
 }
